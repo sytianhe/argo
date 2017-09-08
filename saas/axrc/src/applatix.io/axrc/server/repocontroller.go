76d3974b84656a1a770441cdc193b44accae6dae
Tianhe Zhang <tianhe@applatix.com>
2017-09-08 11:07:04 -0700

    Issue #148: add logic for repo update in axrc

--- saas/axrc/src/applatix.io/axrc/server/repocontroller.go
+++ saas/axrc/src/applatix.io/axrc/server/repocontroller.go
@@ -8,6 +8,13 @@ import (
 	"time"
 )
 
+type Repo struct {
+	Username   string   `json:"username,omitempty"`
+	Protocol   string   `json:"protocol,omitemtpy"`
+	Repos      []string `json:"repos,omitempty"`
+	UseWebhook *bool    `json:"use_webhook,omitempty"`
+}
+
 func PingHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.JSON(axerror.REST_STATUS_OK, "pong")
@@ -21,17 +28,40 @@ func UpdateRepos() {
 		tools, axErr := tool.GetToolsByCategory("scm")
 		if axErr != nil {
 			utils.ErrorLog.Printf("Failed to retrieve tools (err: %v)", axErr)
+			continue
 		}
 
+		listRepos := []Repo{}
 		for _, t := range tools {
-			utils.InfoLog.Println(t)
+			configType := t.GetType()
+			utils.InfoLog.Println(configType)
+
+			if configType == tool.TypeGIT {
+				configObj, ok := t.(*tool.GitConfig)
+				if !ok {
+					utils.ErrorLog.Printf("Failed to convert to git config, %v", t.GetID())
+					continue
+				}
+				r := Repo {
+					Username: configObj.Username,
+					Protocol: configObj.Protocol,
+					Repos: configObj.Repos,
+					UseWebhook: configObj.UseWebhook,
+				}
+			}
+
+			githubT, ok := t.(*tool.GitHubConfig)
+			if !ok {
+				utils.ErrorLog.Printf("Failed to convert to Github config, %v", t.GetID())
+				continue
+			}
+			utils.InfoLog.Println(githubT.Repos)
 			//if t.GetType() == "BitbucketConfig" {
 			//	t.(tool.GitHubConfig).Repos
 			//} else if t.GetType() == "Bitbucket" {
 			//	t.(tool.BitbucketConfig)
 			//}
 			//
-
 		}
 	}
 }
